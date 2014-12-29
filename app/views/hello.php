5b02deb94e793f4ef811c494d46ad25c4825a1fa
“PrinceSyed” <ibrahim48089@yahoo.com>
2014-12-29 01:22:15 -0500

    Commit1

--- app/views/hello.php
+++ app/views/hello.php
@@ -3,6 +3,7 @@
 	<div class="welcome">
         <img src="http://www.seekmomentum.com/wp-content/themes/momentum/images/logo.png" alt="Momentum Ping Pong App">
 		<h1>Let's Build a Ping Pong App</h1>
+		<h1>Let's Build a Ping Pong App</h1>
 	</div>
 
 <?php if(DB::connection()->getDatabaseName())
